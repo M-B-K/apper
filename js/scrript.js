fb4353e5651c21cfd7a11fb76af7f0473a6e0a2f
M-B-K <mohammadbaherk@gmail.com>
2022-04-08 02:55:23 +0300

    new

--- js/scrript.js
+++ js/scrript.js
@@ -1,10 +1,8 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  myFunction();
-};
-
 // Get the navbar
-var navbar = document.getElementById("myHeader");
+let navbar = document.getElementById("myHeader");
+let span = document.querySelector(".up");
+
+
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
@@ -13,4 +11,19 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+  this.scrollY >= 200?span.classList.add("show"):span.classList.remove("show");
+
+};
+
+// for up scroll
+span.onclick = function(){
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    })
 }
