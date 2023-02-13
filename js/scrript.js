aaaa2c24c0d134426bef741cea2a2f0c27a564aa
MBK <mohammadbaherk@gmail.com>
2023-02-13 21:41:42 +0200

    new

--- js/scrript.js
+++ js/scrript.js
@@ -2,11 +2,15 @@
 let navbar = document.getElementById("myHeader");
 let span = document.querySelector(".up");
 
-
+// respnsece menu
+let icon = document.querySelector(".hero .nav-m .icon");
+icon.addEventListener("click", () => {
+  icon.classList.toggle("active");
+});
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= 10) {
+  if (window.pageYOffset >= 600) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
@@ -16,14 +20,15 @@ function myFunction() {
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () {
   myFunction();
-  this.scrollY >= 200?span.classList.add("show"):span.classList.remove("show");
-
+  this.scrollY >= 200
+    ? span.classList.add("show")
+    : span.classList.remove("show");
 };
 
 // for up scroll
-span.onclick = function(){
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
-}
+span.onclick = function () {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+};
