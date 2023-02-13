08abc9fa262ea4518b4da5aec6c52f41a2fa4508
MBK <mohammadbaherk@gmail.com>
2023-02-13 22:32:05 +0200

    new.1

--- js/scrript.js
+++ js/scrript.js
@@ -1,19 +1,30 @@
 // Get the navbar
 let navbar = document.getElementById("myHeader");
 let span = document.querySelector(".up");
+let nav = document.querySelector(".hero .navbar");
 
 // respnsece menu
 let icon = document.querySelector(".hero .nav-m .icon");
 icon.addEventListener("click", () => {
   icon.classList.toggle("active");
+  if (nav.style.background != "rgb(3, 186, 213)") {
+    nav.style.background = "rgb(3, 186, 213)";
+  } else {
+    if (window.pageYOffset <= 600) {
+      nav.style.background = "transparent";
+    }
+  }
 });
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= 600) {
     navbar.classList.add("sticky");
+    nav.style.background = "rgb(3, 186, 213)";
   } else {
     navbar.classList.remove("sticky");
+    if (!icon.classList.contains("active"))
+      nav.style += "background: transparent !important";
   }
 }
 
